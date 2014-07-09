23f0a60780614fd5c303a78687ba070b50760464
Artem Zinoviev <arzinoviev@gmail.com>
2014-07-09 14:46:35 +0300

    change some view

---
+++ app/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+
+@section('title')
+auth | index
+@stop
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <h1 class="text-center">it works!</h1>
+    </div>
+</div>
+@stop
