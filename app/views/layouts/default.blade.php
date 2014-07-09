ae41fb4df5463861b0205216706165cae881ee41
Artem Zinoviev <arzinoviev@gmail.com>
2014-07-09 15:32:48 +0300

    Add Heroku creds and update default route to hit the DB.

--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -21,7 +21,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Cloud Manager </a>
+            <a class="navbar-brand" href="/">Cloud Manager </a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
